521f63461bf904155061b3019d2f4e6ed6a4ccbf
Oussama Benramdane <mr.benramdane.oussama@gmail.com>
2021-06-04 23:51:04 +0200

    Setting up react router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -6,24 +7,30 @@ const Header = () => {
     <header>
       <Navbar bg='light' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            <img
-              alt=''
-              src='.\JMB.png'
-              width='100'
-              height='40'
-              className='d-inline-block align-top'
-            />
-          </Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img
+                alt=''
+                src='../public/images/JMB.png'
+                width='100'
+                height='40'
+                className='d-inline-block align-top'
+              />
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i> Sing In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sing In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
