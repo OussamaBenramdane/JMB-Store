388af7da2708fd24e1afce3010f22d610d935545
Oussama Benramdane <mr.benramdane.oussama@gmail.com>
2021-06-04 21:18:35 +0200

    Starting the frontendV0.1

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
-  return <header>header</header>;
+  return (
+    <header>
+      <Navbar bg='light' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>
+            <img
+              alt=''
+              src='.\JMB.png'
+              width='100'
+              height='40'
+              className='d-inline-block align-top'
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart'></i> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i> Sing In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
