7a7cdd58769c60f7dbfa1833f43462f1cb71619e
Oussama Benramdane <mr.benramdane.oussama@gmail.com>
2021-06-17 02:40:23 +0200

    Setting up backend Autentification

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,6 +28,41 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+//@decs Regester a new user
+//@route POST /api/users
+//@access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({
+    email: email,
+  });
+  if (userExists) {
+    restart.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      isProd: user.isProd,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 //@decs Get user profile
 //@route GET /api/users/profile
 //@access Private
@@ -48,4 +83,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
